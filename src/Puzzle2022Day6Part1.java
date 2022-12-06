4707b5e072ecfb6e7e85c9b1fa911655e4169498
Jan De Laet <jan.delaet@hotmail.com>
2022-12-06 22:03:31 +0100

    Solved puzzle for day 6 of 2022

---
+++ src/Puzzle2022Day6Part1.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.LinkedList;
+
+public class Puzzle2022Day6Part1 {
+    public static void main(String[] args) throws IOException {
+        Path path = Paths.get("resources/input2022_06");
+
+        int count = 1;
+        LinkedList<Integer> list = new LinkedList<>();
+
+        try (BufferedReader reader = Files.newBufferedReader(path)) {
+            while (reader.ready()) {
+                int read = reader.read();
+                if (list.contains(read)) {
+                    int i = list.indexOf(read);
+                    for (int j = 0; j <= i; j++) {
+                        list.pop();
+                    }
+                }
+                list.add(read);
+
+                if (list.size() == 4) {
+                    break;
+                }
+                count++;
+            }
+        }
+        System.out.println("Answer: " + count);
+    }
+}
